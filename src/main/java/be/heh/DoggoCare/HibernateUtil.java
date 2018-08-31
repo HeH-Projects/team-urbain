e9e3309e70512f489119d97822619f3ced669e27
Thomas Rosi <thomas.rosi@skynet.be>
2018-08-31 18:06:22 +0200

    HibernateUtil.java + application.properties

---
+++ src/main/java/be/heh/DoggoCare/HibernateUtil.java
@@ -0,0 +1,28 @@
+package be.heh.DoggoCare;
+
+import java.lang.NullPointerException;
+import javax.persistence.EntityManagerFactory;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HibernateUtil {
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
+    @Bean(destroyMethod="")
+    public SessionFactory getSessionFactory() {
+        SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+        if (sessionFactory == null) {
+            throw new NullPointerException("Factory␣is␣not␣a␣hibernate␣factory.");
+        }
+        return sessionFactory;
+    }
+
+}
+
+
+
