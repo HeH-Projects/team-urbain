ef06b8d419c7a41cc66c922e8ded9e35639279c4
Thomas Rosi <thomas.rosi@skynet.be>
2018-08-31 16:32:52 +0200

    initializing project

---
+++ src/main/java/be/heh/DoggoCare/DoggoCareApplication.java
@@ -0,0 +1,12 @@
+package be.heh.DoggoCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggoCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggoCareApplication.class, args);
+	}
+}
