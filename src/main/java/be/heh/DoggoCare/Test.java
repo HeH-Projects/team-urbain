4aa2659d4e1177aba5b732b43d4ef908f26eb39a
unknown <arnaud.urbain@realdolmen.com>
2018-08-31 21:46:38 +0200

    test 2

--- src/main/java/be/heh/DoggoCare/Test.java
+++ src/main/java/be/heh/DoggoCare/Test.java
@@ -1,4 +1,12 @@
 package be.heh.DoggoCare;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
 public class Test {
+    @RequestMapping("/")
+    public String index() {
+        return "Message de réussite !";
+    }
 }
