55f0b7b59da95820664f0609d060fe36995dad68
unknown <arnaud.urbain@realdolmen.com>
2018-09-02 11:17:47 +0200

    create first repo interface

--- src/main/java/be/heh/DoggoCare/Repository/AppointmentRepository.java
+++ src/main/java/be/heh/DoggoCare/Repository/AppointmentRepository.java
@@ -1,4 +1,19 @@
 package be.heh.DoggoCare.Repository;
 
-public interface AppointmentRepository {
+import be.heh.DoggoCare.Model.Appointment;
+import be.heh.DoggoCare.Model.Customer;
+import be.heh.DoggoCare.Model.Patient;
+import be.heh.DoggoCare.Model.Veterinarian;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    List<Appointment> findByDate(@Param("date") LocalDate date);
+    List<Appointment> findByPatient(@Param("patient") Patient patient);
+    List<Appointment> findByCustomer(@Param("customer") Customer customer);
+    List<Appointment> findByVeterinarian(@Param("veterinarian") Veterinarian veterinarian);
 }
