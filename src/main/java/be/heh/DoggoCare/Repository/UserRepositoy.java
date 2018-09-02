35c789c8c1f1eeb2d91f85def059882b415ebe74
Dexnaw <arnaud-3@outlook.com>
2018-09-02 11:59:57 +0200

    add UserRepository interface

---
+++ src/main/java/be/heh/DoggoCare/Repository/UserRepositoy.java
@@ -0,0 +1,14 @@
+package be.heh.DoggoCare.Repository;
+
+import be.heh.DoggoCare.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepositoy extends JpaRepository<User, Long> {
+
+    List<User> findByFirstName(@Param("firstName") String firstName);
+    List<User> findByLastName(@Param("lastName") String lastName);
+    List<User> findByMail(@Param("mail") String mail);
+}
