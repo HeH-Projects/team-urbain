3410b9b19e41bee1a7a9a63815821c2339f9d932
Dexnaw <arnaud-3@outlook.com>
2018-09-02 11:40:53 +0200

    add CustomerRepository interface

---
+++ src/main/java/be/heh/DoggoCare/Repository/CustomerRepository.java
@@ -0,0 +1,19 @@
+package be.heh.DoggoCare.Repository;
+
+import be.heh.DoggoCare.Model.Appointment;
+import be.heh.DoggoCare.Model.Customer;
+import be.heh.DoggoCare.Model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    List<Customer> findByFirstName(@Param("firstName") String firstName);
+    List<Customer> findByLastName(@Param("lastName") String lastName);
+    List<Customer> findByMail(@Param("mail") String mail);
+    List<Customer> findByPhone(@Param("phone") String phone);
+    List<Customer> findByPatient(@Param("patient")Patient patient);
+    List<Customer> findByAppointment(@Param("appointment")Appointment appointment);
+}
