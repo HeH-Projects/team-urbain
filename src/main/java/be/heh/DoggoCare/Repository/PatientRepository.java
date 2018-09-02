0c0d489d1fd5c0032a050b2f5de1a9809be9eb0c
Dexnaw <arnaud-3@outlook.com>
2018-09-02 11:51:53 +0200

    add PatientRepository interface

--- src/main/java/be/heh/DoggoCare/Repository/PatientRepository.java
+++ src/main/java/be/heh/DoggoCare/Repository/PatientRepository.java
@@ -1,4 +1,17 @@
 package be.heh.DoggoCare.Repository;
 
-public class PatientRepository {
+import be.heh.DoggoCare.Model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    List<Patient> findByName(@Param("name") String name);
+    List<Patient> findByDogBreed(@Param("dogBreed") String dogBreed);
+    List<Patient> findBySize(@Param("size") double size);
+    List<Patient> findByWeight(@Param("weight") double weight);
+    List<Patient> findByAge(@Param("age") int age);
+
 }
