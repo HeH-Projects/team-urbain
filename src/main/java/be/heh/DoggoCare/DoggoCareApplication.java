9f565d2139e1679f4c8124ddd7a26bcffdbe096e
Dexnaw <arnaud-3@outlook.com>
2018-09-03 13:12:30 +0200

    add hibernate dependency

--- src/main/java/be/heh/DoggoCare/DoggoCareApplication.java
+++ src/main/java/be/heh/DoggoCare/DoggoCareApplication.java
@@ -1,9 +1,17 @@
 package be.heh.DoggoCare;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableTransactionManagement
+@EnableJpaRepositories
+@EnableScheduling
 public class DoggoCareApplication {
 
 	public static void main(String[] args) {
