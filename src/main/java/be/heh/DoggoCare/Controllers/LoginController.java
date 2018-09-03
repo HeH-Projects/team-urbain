9a0696308528cd2d7c932e4f4bd997a588bfce8d
Dexnaw <arnaud-3@outlook.com>
2018-09-03 14:54:35 +0200

    change mapping for index.html

--- src/main/java/be/heh/DoggoCare/Controllers/LoginController.java
+++ src/main/java/be/heh/DoggoCare/Controllers/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-    @GetMapping(value = "/login")
+    @GetMapping(value = "/")
     public String login(){
         return "index.html";
     }
